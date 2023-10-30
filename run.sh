390435812cc55b4e70027fdd567484067931a294
MartinStaab <10105064-MartinStaab@users.noreply.replit.com>
2023-10-30 19:16:09 +0000

    Changed: split run.sh into two files, added profiling result to .gitignore

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
-# required, so we don't run anything if compilation fails
-rm App.exe 2> /dev/null 
-
-find ./*/ -name "*.cs" -exec csc -debug App.cs {} +
+bash compile.bash
 
 if [ -f App.exe ]; then
     mono --debug App.exe
