4786937f4c573078d003f9393d69aa61b16299b9
budopod1 <51675874+budopod1@users.noreply.github.com>
2023-11-03 18:01:50 +0000

    Moved: files in Tokens directory to subdirectories

--- Tokens/Abstract/Block.cs
+++ Tokens/Abstract/Block.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-public class Block : TreeToken {
+public abstract class Block : TreeToken {
     public Block(List<IToken> tokens) : base(tokens) {}
     
     protected override TreeToken _Copy(List<IToken> tokens) {
