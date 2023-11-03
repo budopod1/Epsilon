7eaef27bdde5862ef8ea45093686a079efa0bb48
budopod1 <51675874+budopod1@users.noreply.github.com>
2023-11-03 18:28:12 +0000

    Oops: made Block no longer abstract (how did I not catch that)

--- Tokens/Other/Block.cs
+++ Tokens/Other/Block.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Collections.Generic;
 
-public abstract class Block : TreeToken {
+public class Block : TreeToken {
     public Block(List<IToken> tokens) : base(tokens) {}
-    
+
     protected override TreeToken _Copy(List<IToken> tokens) {
-        return (TreeToken)new Block(tokens);
+        return new Block(tokens);
     }
 }
