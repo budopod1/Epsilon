3c6df515a61c40372f26a3c7dab1d72015555c61
MartinStaab <10105064-MartinStaab@users.noreply.replit.com>
2023-11-07 23:56:03 +0000

    Changed: converted ParameterGroup to UnaryOperation

--- Tokens/Values/ParameterGroup.cs
+++ Tokens/Values/ParameterGroup.cs
@@ -1,34 +1,14 @@
 using System;
 using System.Collections.Generic;
 
-public class ParameterGroup : IParentToken, IValueToken {
-    public IParentToken parent { get; set; }
-    public CodeSpan span { get; set; }
-    
-    public int Count {
-        get { return 1; }
-    }
-    
-    public IToken this[int i] {
-        get { return child; }
-        set { child = (IValueToken)value; }
-    }
-
-    IValueToken child;
-    
-    public ParameterGroup(IValueToken child) {
-        this.child = child;
-    }
+public class ParameterGroup : UnaryOperation<IValueToken>, IValueToken {
+    public ParameterGroup(IValueToken o) : base(o) {}
     
     public Type_ GetType_() {
-        return child.GetType_();
-    }
-
-    public override string ToString() {
-        return Utils.WrapName(GetType().Name, child.ToString());
+        return o.GetType_();
     }
 
     public int Serialize(SerializationContext context) {
-        return child.Serialize(context);
+        return o.Serialize(context);
     }
 }
