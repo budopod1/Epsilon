f83037f8d162b33acd1ae444299f81f8520d4f1a
MartinStaab <10105064-MartinStaab@users.noreply.replit.com>
2023-11-09 23:09:02 +0000

    Fixed: proper c libraries are now installed for LLVM

--- replit.nix
+++ replit.nix
@@ -1,8 +1,15 @@
 { pkgs }: {
+    env = {
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.stdenv.cc.cc.lib
+        ];
+    };
     deps = [
         pkgs.python39Packages.virtualenv
         pkgs.ack
         pkgs.python310
         pkgs.mono5
+        pkgs.coreutils
+        pkgs.git
     ];
 }
