225bd72fc054c1deb7b92cda71c9496bca538344
budopod1 <51675874+budopod1@users.noreply.github.com>
2023-11-10 05:01:18 +0000

    Added: output to file, structs and arrays now act like reference types, arrays now have length field | Fixed: set_body on IdentifiedStructType missing *

--- LLVMIR/_create_ir.py
+++ LLVMIR/_create_ir.py
@@ -6,6 +6,23 @@ import orjson
 REF_COUNTER_FIELD = ir.IntType(32)
 
 
+class Array:
+    def __init__(self, program, id_, type_):
+        self.program = program
+        self.id_ = id_
+        self.type_ = type_
+        self.generic = type_["generics"][0]
+        field_ir_types = [
+            REF_COUNTER_FIELD, 
+            ir.IntType(32), 
+            ir.PointerType(create_type_(program, self.generic))
+        ]
+        self.ir_type = ir.LiteralStructType(field_ir_types).as_pointer()
+        ir.global_context.get_identified_type(
+            "___a"+str(id_)
+        ).set_body(*field_ir_types)
+
+
 class Struct:
     def __init__(self, program, name, fields):
         self.program = program
@@ -15,10 +32,10 @@ class Struct:
             create_type_(program, field["type_"])
             for field in fields
         ]
-        self.ir_type = ir.LiteralStructType(field_ir_types)
+        self.ir_type = ir.LiteralStructType(field_ir_types).as_pointer()
         ir.global_context.get_identified_type(
             name
-        ).set_body(field_ir_types)
+        ).set_body(*field_ir_types)
 
 
 class Function:
@@ -36,22 +53,6 @@ class Function:
         )
         self.ir = ir.Function(program.module, self.ir_type,
                               name="___f"+str(id_))
-
-
-class Array:
-    def __init__(self, program, id_, type_):
-        self.program = program
-        self.id_ = id_
-        self.type_ = type_
-        self.generic = type_["generics"][0]
-        field_ir_types = [
-            REF_COUNTER_FIELD, 
-            ir.PointerType(create_type_(program, self.generic))
-        ]
-        self.ir_type = ir.LiteralStructType(field_ir_types)
-        ir.global_context.get_identified_type(
-            "___a"+str(id_)
-        ).set_body(field_ir_types)
 
 
 class Program:
@@ -98,7 +99,7 @@ def create_type_(program, data):
         case "Void", []:
             return ir.VoidType()
         case "Pointer", [pointee]:
-            return ir.PointerType(pointee)
+            return pointee.as_pointer()
         case "Array", [sub]:
             id_ = program.array_ids[freeze_json(data)]
             for id_ in program.arrays:
@@ -106,12 +107,14 @@ def create_type_(program, data):
             else:
                 return ir.global_context.get_identified_type(
                     "___a"+str(id_)
-                )
+                ).as_pointer()
         case name, []:
             if name in program.structs:
                 return program.structs[name].ir_type
             else:
-                return ir.global_context.get_identified_type(name)
+                return ir.global_context.get_identified_type(
+                    name
+                ).as_pointer()
     
     raise TypeError(f"Invalid type {data}")
 
@@ -173,7 +176,8 @@ def main():
 
     module = create_ir(data)
 
-    print(module)
+    with open("code.ll", "w") as file:
+        file.write(str(module))
 
 
 if __name__ == "__main__":
