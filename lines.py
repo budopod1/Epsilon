bb6042081626c0a6d0b460c9a263be6117948cfe
budopod1 <51675874+budopod1@users.noreply.github.com>
2023-11-22 20:54:37 +0000

    Improved: line counting util

--- lines.py
+++ lines.py
@@ -1,28 +1,59 @@
+import argparse
 from pathlib import Path
 
 
 FILE_EXTENSIONS = ["py", "nix", "cs", "ε", "ep", "eps", "epsilon", "bash"]
-IGNORE_NAMES = []
-IGNORE_FOLDERS = ["venv"]
+IGNORE = ["venv"]
 
 
-def main():
+def main(*, verbosity, extentions, ignore):
     files = sum([
         list(Path.cwd().glob("**/*."+FILE_EXTENSION))
-        for FILE_EXTENSION in FILE_EXTENSIONS
+        for FILE_EXTENSION in extentions
     ], [])
     lines = 0
     for file in files:
-        if file.name in IGNORE_NAMES:
+        if file.name in ignore:
+            if verbosity >= 2:
+                print(f"Skipping file {file}, it has an ignored name")
             continue
-        if any(folder in file.parts for folder in IGNORE_FOLDERS):
+        if any(folder in file.parts for folder in ignore):
+            if verbosity >= 2:
+                print(f"Skipping file {file}, it is in an ignored folder")
             continue
         txt = file.read_text()
         file_lines = 1 + txt.count("\n")
-        print(file, file_lines)
+        if verbosity >= 1:
+            print(file, "has", file_lines, "lines")
         lines += file_lines
     print("The code has", lines, "lines")
 
 
+def en_list(items):
+    if len(items) == 0:
+        return "none"
+    if len(items) == 1:
+        return items[0]
+    if len(items) == 2:
+        return " and ".join(items)
+    return ", ".join(items[:-1]) + ", and " + items[-1]
+
+
+def cmdline():
+    parser = argparse.ArgumentParser(
+        prog="Line Counter",
+        description=f"Count the lines in this project, in files with extention(s) {en_list(list(map((lambda ext: '.'+ext), FILE_EXTENSIONS)))}, that do not have the name and are not in the folders {en_list(IGNORE)}."
+    )
+    parser.add_argument("-v", "--verbosity", type=int, default=0, choices=range(0, 3))
+    parser.add_argument("-e", "--extensions", help="override file extentions to include", action="extend", type=str, nargs="*")
+    parser.add_argument("-i", "--ignore", help="override files and folders to ignore", action="extend", type=str, nargs="*")
+
+    args = parser.parse_args()
+
+    extentions = args.extensions or FILE_EXTENSIONS
+    ignore = args.ignore or IGNORE
+    main(verbosity=args.verbosity, extentions=extentions, ignore=ignore)
+
+
 if __name__ == "__main__":
-    main()
+    cmdline()
