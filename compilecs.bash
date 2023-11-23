fcd683143ba9744ef8877fb5e3ed91f7fcb5f873
budopod1 <51675874+budopod1@users.noreply.github.com>
2023-11-23 02:24:34 +0000

    Updated: shell scripts

--- compilecs.bash
+++ compilecs.bash
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-# Compile C#
-
-# required, so we don't run anything if compilation fails
-rm -f App.exe
-
 find ./*/ -name "*.cs" -exec csc -debug App.cs {} +
--- executedebug.bash
+++ executedebug.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ -f App.exe ]; then
-    mono --debug App.exe
+    mono --debug App.exe compile $1 code
 fi
