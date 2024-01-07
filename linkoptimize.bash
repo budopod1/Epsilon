8ed46f77d3c01fa829398bd3209c3c6cd8b9d2fe
budopod1 <51675874+budopod1@users.noreply.github.com>
2024-01-07 22:46:09 +0000

    Removed: unnecessary call to llvm-as in linkoptimize.bash

--- linkoptimize.bash
+++ linkoptimize.bash
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
-llvm-as code.ll -o code.bc
-llvm-link code.bc builtins.bc -o code-linked.bc
+llvm-link code.ll builtins.bc -o code-linked.bc
 opt -O3 code-linked.bc -o code-opt.bc
