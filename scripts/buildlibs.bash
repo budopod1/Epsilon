93166a968b2ece7e8d93f489185c877767fc60e9
Martin Staab <martin.e.staab@gmail.com>
2024-10-20 16:05:19 -0700

    Changed: libs now are built specifically without opaque pointers

--- scripts/buildlibs.bash
+++ scripts/buildlibs.bash
@@ -4,9 +4,9 @@ cd "${0%/*}/.."
 
 for path in ./libs/*.c; do
     file="${path%.*}"
-    clang -S -emit-llvm $path -o "${file}.ll"
-    opt -O3 "${file}.ll" -o "${file}.bc"
-    llc "${file}.bc" -o "${file}.o" -filetype=obj
+    clang -S -emit-llvm $path -Xclang -no-opaque-pointers -o "${file}.ll"
+    opt -O3 "${file}.ll" -opaque-pointers=0 -o "${file}.bc"
+    llc "${file}.bc" -opaque-pointers=0 -o "${file}.o" -filetype=obj
     if [ "${file##*/}" != "builtins" ]; then
         rm "${file}.ll"
     fi
