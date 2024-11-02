e658097dfe7939d55eaaa3940d42a72fde97bc6c
Martin Staab <martin.e.staab@gmail.com>
2024-11-02 15:47:12 -0700

    Fixed: epslc.bash and buildlibs.bash now work when Epsilon's path includes spaces

--- scripts/buildlibs.bash
+++ scripts/buildlibs.bash
@@ -4,7 +4,7 @@ cd "${0%/*}/.."
 
 for path in ./libs/*.c; do
     file="${path%.*}"
-    clang -S -emit-llvm $path -o "${file}.ll"
+    clang -S -emit-llvm "$path" -o "${file}.ll"
     opt -O3 "${file}.ll" -o "${file}.bc"
     llc "${file}.bc" -o "${file}.o" -filetype=obj -O=0
     if [ "${file##*/}" != "builtins" ]; then
--- scripts/epslc.bash
+++ scripts/epslc.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-script_path=$(realpath $0)
-executable_path="${script_path%/*}/../bin/*/net8.0/EpsilonLang.dll"
-dotnet $executable_path "$@"
+script_path=$(realpath "$0")
+executable_path="${script_path%/*}/../bin/*/net*/EpsilonLang.dll"
+dotnet "$executable_path" "$@"
