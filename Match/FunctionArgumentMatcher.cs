c8d1d043bc8af7cb3c17cef7c1af43981a919f24
Martin Staab <martin.e.staab@gmail.com>
2024-11-25 11:27:19 -0800

    Changed: function arguments in template to use [], not <> | Added: migration script

--- Match/FunctionArgumentMatcher.cs
+++ Match/FunctionArgumentMatcher.cs
@@ -1,30 +1,43 @@
 namespace Epsilon;
 public class FunctionArgumentMatcher : IMatcher {
     public Match Match(IParentToken tokens) {
-        for (int i = 0; i < tokens.Count-1; i++) {
-            bool first = true;
-            int indentCount = 0;
-            List<IToken> replaced = [];
-            List<IToken> replacementTokens = [];
-            for (int j = i; j < tokens.Count; j++) {
-                IToken token = tokens[j];
-                if (token is TextToken ttoken) {
-                    string text = ttoken.GetText();
-                    if ((text != "<") && first) break;
-                    if (text == "<") indentCount++;
-                    if (text == ">") indentCount--;
-                    if (indentCount == 0) {
-                        return new Match(i, j, [
-                            new RawFunctionArgument(replacementTokens)
+        int indent = 0;
+        List<IToken> replaced = null;
+
+        for (int i = 0; i < tokens.Count; i++) {
+            IToken token = tokens[i];
+
+            if (token is TextToken ttoken) {
+                string text = ttoken.GetText();
+                if (text == "[") {
+                    if (indent == 0) {
+                        replaced = [];
+                    }
+                    indent++;
+                } else if (text == "]") {
+                    indent--;
+                    if (indent < 0) {
+                        throw new SyntaxErrorException(
+                            "Closing square bracket does not have a matching opening square bracket", token
+                        );
+                    } else if (indent == 0) {
+                        return new Match(i-replaced.Count, i, [
+                            new RawFunctionArgument(replaced.Skip(1).ToList())
                         ], replaced);
                     }
-                } else if (first) {
-                    break;
                 }
-                if (!first) replacementTokens.Add(token);
-                first = false;
             }
+
+            replaced?.Add(token);
         }
+
+        if (indent > 0) {
+            throw new SyntaxErrorException(
+                "Expected closing square backet",
+                new CodeSpan(tokens[^1].span.GetEnd())
+            );
+        }
+
         return null;
     }
 }
