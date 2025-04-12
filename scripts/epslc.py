2d5143d1adfa6a8b4a4b313e5e58931c624c30c6
Martin Staab <martin.e.staab@gmail.com>
2025-04-12 10:58:24 -0700

    Fixed: epslc not running when not in Epsilon project root | Bumped: C-Run-Command version

--- scripts/epslc.py
+++ scripts/epslc.py
@@ -3,7 +3,7 @@ from scriptutils import *
 
 
 def epslc(args):
-    run_cmd("dotnet", Path("executables/Epsilon.dll").absolute(),
+    run_cmd("dotnet", (get_project_root() / "executables/Epsilon.dll").absolute(),
         *args, use_dash_x_flag=False)
 
 
